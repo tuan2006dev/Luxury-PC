625448e7b2e6185fcfe31ff559bdf57c17e41632
Your Name <you@example.com>
2026-06-21 19:07:08 +0700

    Remove garbage .py and .log files

--- script_del.py
+++
@@ -1,6 +0,0 @@
-
-import sys
-lines = open('src/main/resources/templates/index.html', encoding='utf-8').readlines()
-new_lines = lines[:405] + lines[736:]
-open('src/main/resources/templates/index.html', 'w', encoding='utf-8').writelines(new_lines)
-
