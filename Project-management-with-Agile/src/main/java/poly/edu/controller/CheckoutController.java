da909bd155012ff8110522c1fc24605a402d7782
Phạm Công Thanh <phamcongthanh.8311@gmail.com>
2026-06-23 09:39:14 +0700

    i18n

--- Project-management-with-Agile/src/main/java/poly/edu/controller/CheckoutController.java
+++
@@ -1,8 +0,0 @@
-package poly.edu.controller;
-
-// CheckoutController đã được hợp nhất vào CartController (Van's version)
-// GET /checkout giờ được xử lý bởi CartController.viewCheckout()
-// để tránh conflict mapping, class này được để trống
-// @Controller
-// public class CheckoutController { ... }
-
